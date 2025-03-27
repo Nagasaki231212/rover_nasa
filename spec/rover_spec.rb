c1e68b31bf7c6b6fe0920e25d97010381932e857
oscar gil <mcastrorap@gmail.com>
2025-03-27 19:01:21 -0400

    actualize el test

--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,20 +1,10 @@
 require_relative "../lib/rover_nasa/rover"
 
 RSpec.describe Rover do
-  it "Mover el rover con indicaciones 🚀" do
+  it "Mover el rover con pura fuerza de voluntad 🇻🇪" do
     rover = Rover.new(1, 2, 'N') 
-    indicaciones = "LMLMLMLMM".chars
 
-    indicaciones.each do |indicacione|
-      if indicacione == 'L'
-        rover.girar_a_la_izquierda
-      elsif indicacione == 'R'
-        rover.girar_a_la_derecha
-      elsif indicacione == 'M'
-        rover.mover_rovers
-      end
-    end
-
+    rover.ejecutar_comandos("LMLMLMLMM") 
     expect(rover.to_s).to eq("1, 3, N") 
   end
 end
